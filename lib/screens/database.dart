9b412eebb47d8c2623dc0e37b97708badcc95621
Adrian Jagielak <adrianjagielak@gmail.com>
2021-01-14 19:54:48 +0100

    Create Values, Loved, Database and Settings screens

---
+++ lib/screens/database.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Database extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Database!'),
+    );
+  }
+}
---
+++ lib/screens/loved.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Loved extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Loved!'),
+    );
+  }
+}
