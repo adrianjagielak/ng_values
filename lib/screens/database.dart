8f7d56a47eb9a5c9cd8a598c9b7730ebadd6c94e
Adrian Jagielak <adrianjagielak@gmail.com>
2021-01-15 00:47:44 +0100

    Add DatabaseListItem and simple displaying the values in Database screen

--- lib/screens/database.dart
+++ lib/screens/database.dart
@@ -1,14 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../services/providers/values_provider.dart';
+import '../widgets/database_list_item.dart';
 
 class Database extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    ValuesProvider valuesProvider = Provider.of(context);
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Database'),
       ),
-      body: Center(
-        child: Text('Database!'),
+      body: ListView(
+        children: valuesProvider.values
+            .map((e) => DatabaseListItem(value: e))
+            .toList(),
       ),
     );
   }
---
+++ lib/widgets/database_list_item.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../models/value.dart';
+import '../services/providers/values_provider.dart';
+
+class DatabaseListItem extends StatelessWidget {
+  DatabaseListItem({
+    @required this.value,
+  });
+
+  final Value value;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Row(
+          children: [
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                child: Text(
+                  value.text,
+                ),
+              ),
+            ),
+            if (value.canBeDeleted)
+              IconButton(
+                icon: Icon(Icons.clear),
+                onPressed: () {
+                  showDialog(
+                    context: context,
+                    barrierDismissible: true,
+                    builder: (BuildContext context) {
+                      return AlertDialog(
+                        title: Text('Warning!'),
+                        content: Text(
+                          'Are you sure you want to erase this precious quote '
+                          'from history?',
+                        ),
+                        actions: [
+                          FlatButton(
+                            onPressed: () => Navigator.of(context).pop(),
+                            child: Text('CANCEL'),
+                          ),
+                          FlatButton(
+                            onPressed: () {
+                              Provider.of<ValuesProvider>(context,
+                                      listen: false)
+                                  .remove(value.id);
+                              Navigator.of(context).pop();
+                            },
+                            child: Text('OK'),
+                          ),
+                        ],
+                      );
+                    },
+                  );
+                },
+              )
+            else
+              SizedBox(height: 48),
+          ],
+        ),
+      ),
+    );
+  }
+}
