9ea6f1c571e43e72da3ce28c4996a872f33d3679
Adrian Jagielak <adrianjagielak@gmail.com>
2021-01-16 18:05:12 +0100

    Remove unused import in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 import 'screens/home.dart';
 import 'services/providers/favorites_provider.dart';
